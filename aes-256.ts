509f6be01164e2996c92b3f0b1e39b0f08e801f7
Brack0 <denis.souron@laposte.net>
2018-03-05 14:07:13 +0100

    First real commit

--- aes-256.ts
+++ aes-256.ts
@@ -1,3 +1,16 @@
-const Nb: number = 4;
-const Nk: number = 8;
-const Nr: number = 14;
+abstract class ALGORITHM_PARAMETERS {
+    /**
+     * Number of columns (32-bit words) comprising the State. Standard value : 4
+     */
+    static Nb: number = 4;
+
+    /**
+     * Number of 32-bit words comprising the Cipher Key. Standard value (AES-256) : 8
+     */
+    static Nk: number = 8;
+
+    /**
+     * Number of rounds, which is a function of Nk and Nb (which is fixed). Standard value (AES-256) : 14
+     */
+    static Nr: number = 14;
+}
